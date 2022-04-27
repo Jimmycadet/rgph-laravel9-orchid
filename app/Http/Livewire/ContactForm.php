a81729e39186e9b8e5c8b8608a5db6096cebc50b
Jimmycadet <79710417+Jimmycadet@users.noreply.github.com>
2022-04-27 09:51:00 -0400

    update contact-form

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Message;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 
 class ContactForm extends Component
@@ -10,6 +12,41 @@ class ContactForm extends Component
     public $email='';
     public $subject='';
     public $comments='';
+
+    protected $rules=[
+        'name'=>'required|min:6',
+        'email'=>'required|email',
+        'subject'=>'required|min:6',
+        'comments'=>'required|min:10',
+    ];
+
+    protected $messages=[
+        'name.required'=>'Votre nom est obligatoire',
+        'name.min'=>'Votre nom doit avoir au moins 6 caractères',
+    ];
+
+    public function updated($propertyName){
+
+            $this->validateOnly($propertyName);
+
+    }
+
+    public function saveMessage(){
+
+        $this->validate();
+
+        Message::create([
+            'name'=>$this->name,
+            'email'=>$this->email,
+            'subject'=>$this->subject,
+            'comments'=>$this->comments,
+        ]);
+        $this->name='';
+        $this->email='';
+        $this->subject='';
+        $this->comments='';
+
+    }
 
     public function render()
     {
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -7,9 +7,14 @@
                 <div class="mb-3">
                     <label class="form-label">Nom <span class="text-danger">*</span></label>
                     <div class="form-icon position-relative">
-                        <i data-feather="user" class="fea icon-sm icons"></i>
-                        <input wire:model="name" name="name" id="name" type="text" class="form-control ps-5" placeholder="Nom :">
+                            <i data-feather="user" class="fea icon-sm icons"></i>
+                        <input wire:model.lazy="name" name="name" id="name" type="text" class="form-control ps-5" placeholder="Nom :">
                     </div>
+                    @error('name')
+                    <div class="text-danger" style="font-size: 10px!important;">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
             </div>
 
@@ -18,8 +23,13 @@
                     <label class="form-label">Email <span class="text-danger">*</span></label>
                     <div class="form-icon position-relative">
                         <i data-feather="mail" class="fea icon-sm icons"></i>
-                        <input wire:model="email" name="email" id="email" type="email" class="form-control ps-5" placeholder="Email :">
+                        <input wire:model.lazy="email" name="email" id="email" type="email" class="form-control ps-5" placeholder="Email :">
                     </div>
+                    @error('email')
+                    <div class="text-danger" style="font-size: 10px!important;">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
             </div><!--end col-->
 
@@ -28,8 +38,13 @@
                     <label class="form-label">Sujet</label>
                     <div class="form-icon position-relative">
                         <i data-feather="book" class="fea icon-sm icons"></i>
-                        <input wire:model="subject" name="subject" id="subject" class="form-control ps-5" placeholder="sujet :">
+                        <input wire:model.lazy="subject" name="subject" id="subject" class="form-control ps-5" placeholder="sujet :">
                     </div>
+                    @error('subject')
+                    <div class="text-danger" style="font-size: 10px!important;">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
             </div><!--end col-->
 
@@ -38,8 +53,13 @@
                     <label class="form-label">Comments <span class="text-danger">*</span></label>
                     <div class="form-icon position-relative">
                         <i data-feather="message-circle" class="fea icon-sm icons clearfix"></i>
-                        <textarea wire:model="comments" name="comments" id="comments" rows="4" class="form-control ps-5" placeholder="Message :"></textarea>
+                        <textarea wire:model.lazy="comments" name="comments" id="comments" rows="4" class="form-control ps-5" placeholder="Message :"></textarea>
                     </div>
+                    @error('comments')
+                    <div class="text-danger" style="font-size: 10px!important;">
+                        {{$message}}
+                    </div>
+                    @enderror
                 </div>
             </div>
         </div>
